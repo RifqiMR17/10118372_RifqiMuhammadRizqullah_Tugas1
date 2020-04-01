7053320b9bfefa0263d44846582a744532fbbb07
Rifqi M_R <Rifqi M_R@LAPTOP-DA6U791I>
2020-04-01 23:00:26 +0700

    Tugas 1 ProVis

---
+++ src/kealssutama.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Rifqi M_R
+ */
+public class kealssutama {
+    public static void main(String[] args) {
+       // System.out.println("hello world");
+       // frameutama Futama = new frameutama();
+        //Futama.tampil("Program Pertama GUI");
+        Futama Utama  = new Futama();
+        //Utama.setTitle("Pemrograman Provis");
+        Utama.setVisible(true);
+    
+    }
+}
